902dd1d5f49001426131adf2793f6ddd8512f706
liaohuangzhen <1969418119@qq.com>
2020-12-25 10:17:56 +0800

    12.25

--- js/2登录.js
+++ js/2登录.js
@@ -1,6 +1,6 @@
 $("#button").click(function(){
 	if($("#input2").val()==""){
-		alert("人丑无法登录")
+		alert("因为人丑，无法登录")
 	}
 	
 	// var Obj = /^[a-zA-Z]\w{3,15}
@@ -14,3 +14,38 @@ $("#button").click(function(){
 	
 })
 
+
+$(document).mouseup(function () { 
+	if (6>5) {
+		// 获取当前页面的宽度（考虑兼容性）
+		var width= 
+		window.innerWidth||
+		document.documentElement.clientWidth||
+		document.body.clientWidth;   
+	
+		//获取当前页面的高度
+		var height = 
+		window.innerHeight||
+		document.documentElement.clientHeight||
+		document.body.clientHeight;
+	
+		console.log(document.body.clientWidth)
+	}
+});
+
+
+
+//当浏览器窗口大小改变时，触发 window.onresize事件
+window.onresize=function(){  
+	//如果页面宽度小于900 $("body").eq(0) 隐藏 $("body").eq(1)显示
+	if (window.innerWidth<900) {
+		$("body").eq(0).hide()
+		$("body").eq(1).show()
+	}else{
+		//如果页面宽度不小于900 $("body").eq(1) 隐藏 $("body").eq(0)显示
+		$("body").eq(1).hide()
+		$("body").eq(0).show()
+	}
+} 
+
+	
